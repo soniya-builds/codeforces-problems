c7c09bca3bb210d06bfd4513ff68d377bd15e665
soniya-builds <soniyasmiles05@gmail.com>
2026-01-07 00:07:48 +0600

    Final Solution

--- OperationswithInversions.py
+++ OperationswithInversions.py
@@ -2,31 +2,11 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    arr = list(map(int, input().split()))
+    a = list(map(int, input().split()))
 
-    max_so_far = arr[0]
-    operations = 0
+    kept = []
+    for x in a:
+        if not kept or x >= kept[-1]:
+            kept.append(x)
 
-    for i in range(1, n):
-        if arr[i] < max_so_far:
-            operations += 1
-        else:
-            max_so_far = arr[i]
-
-    print(operations)
-t = int(input())
-
-for _ in range(t):
-    n = int(input())
-    arr = list(map(int, input().split()))
-
-    max_so_far = arr[0]
-    operations = 0
-
-    for i in range(1, n):
-        if arr[i] < max_so_far:
-            operations += 1
-        else:
-            max_so_far = arr[i]
-
-    print(operations)
+    print(n - len(kept))
